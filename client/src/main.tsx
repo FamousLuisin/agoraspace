2deb7dafcac967689b55655733ef47bd5bac92e5
Luís Filipe <lufimelo@outlook.com>
2025-07-07 20:28:28 -0300

    feat(client): create home screen

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "@/App.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="w-full min-h-screen h-full flex flex-col items-center bg-background dark:bg-background">
+        <App />
+      </div>
+    </ThemeProvider>
   </StrictMode>
 );
